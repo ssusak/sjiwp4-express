6c81f3a03da5696ad31e47eaaf322f0f81459c9a
Senad S <senad.susak1@gmail.com>
2023-10-26 10:27:19 +0200

    Prva promjena

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ app.get("/", (request, response) => {
 response.send("I am alive!");
 });
 
-app.get("/proba/:name", (request, response) => {
-    console.log("Name:", request.params.name);
-    response.send("Good day, " + request.params.name);
-    });
-
 app.listen(port, () => {
     console.log("I am listening...");
 });
