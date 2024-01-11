baa0241b717ee8a8b6174dcb8c33769a82d4e43b
Senad S <senad.susak1@gmail.com>
2024-01-11 12:04:40 +0100

    competitions+signout

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const { engine } = require("express-handlebars");
 // ROUTERS IMPORT
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const competitionsRouter = require('./routes/competitions');
+const { parseAuthCookie } = require('./services/auth');
 
 // APP INIT
 const app = express();
@@ -23,10 +25,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(parseAuthCookie);
 
 // ROUTERS SETUP
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/competitions', competitionsRouter);
 
 // 404 > ERROR HANDLER
 app.use(function (req, res, next) {
@@ -38,6 +42,10 @@ app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  if (err.status === 404) {
+    res.locals.message = "Tražena stranica nije pronađena";
+  }
 
   // render the error page
   res.status(err.status || 500);
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,12 @@ const { db } = require("../services/db.js");
 const { getUserJwt } = require("../services/auth.js");
 const bcrypt = require("bcrypt");
 
+// GET /users/signout
+router.get("/signout", function (req, res, next) {
+  res.clearCookie(process.env.AUTH_COOKIE_NAME);
+  res.redirect("/");
+});
+
 // GET /users/signin
 router.get("/signin", function (req, res, next) {
   res.render("users/signin", { result: { display_form: true } });
@@ -40,7 +46,7 @@ router.post("/signin", function (req, res, next) {
     }
 
     const token = getUserJwt(dbResult.id, dbResult.email, dbResult.name, dbResult.role);
-    res.cookie("auth", token);
+    res.cookie(process.env.AUTH_COOKIE_NAME, token);
 
     res.render("users/signin", { result: { success: true } });
   } else {
@@ -70,10 +76,16 @@ router.post("/signup", function (req, res, next) {
     return;
   }
 
-  const passwordHash = bcrypt.hashSync(req.body.password, 10);
+  const stmt1 = db.prepare("SELECT * FROM users WHERE email = ?;");
+  const selectResult = stmt1.get(req.body.email);
+  if (selectResult) {
+    res.render("users/signup", { result: { email_in_use: true, display_form: true } });
+    return;
+  }
 
-  const stmt = db.prepare("INSERT INTO users (email, password, name, signed_at, role) VALUES (?, ?, ?, ?, ?);");
-  const insertResult = stmt.run(req.body.email, passwordHash, req.body.name, Date.now(), "user");
+  const passwordHash = bcrypt.hashSync(req.body.password, 10);
+  const stmt2 = db.prepare("INSERT INTO users (email, password, name, signed_at, role) VALUES (?, ?, ?, ?, ?);");
+  const insertResult = stmt2.run(req.body.email, passwordHash, req.body.name, Date.now(), "user");
 
   if (insertResult.changes && insertResult.changes === 1) {
     res.render("users/signup", { result: { success: true } });
